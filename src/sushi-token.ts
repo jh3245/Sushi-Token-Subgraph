fa5c39c22b12e47b464464b488a17e8975121c10
Nolan Amblard <ndamblard@gmail.com>
2023-11-27 22:40:07 -0500

    v0.0.1

--- src/sushi-token.ts
+++ src/sushi-token.ts
@@ -1,3 +1,4 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import {
   Approval as ApprovalEvent,
   DelegateChanged as DelegateChangedEvent,
@@ -10,7 +11,9 @@ import {
   DelegateChanged,
   DelegateVotesChanged,
   OwnershipTransferred,
-  Transfer
+  Transfer,
+  TokenHolder,
+  Whale
 } from "../generated/schema"
 
 export function handleApproval(event: ApprovalEvent): void {
@@ -77,6 +80,45 @@ export function handleOwnershipTransferred(
 }
 
 export function handleTransfer(event: TransferEvent): void {
+  let sender = TokenHolder.load(event.params.from.toHexString());
+  if (!sender) {
+    sender = new TokenHolder(event.params.from.toHexString());
+    sender.balance = new BigInt(0);
+  } else {
+    sender.balance = sender.balance.minus(event.params.value);
+  }
+  sender.save();
+
+  let receiver = TokenHolder.load(event.params.to.toHexString());
+  if (!receiver) {
+    receiver = new TokenHolder(event.params.to.toHexString());
+    receiver.balance = event.params.value;
+  } else {
+    receiver.balance = receiver.balance.plus(event.params.value);
+  }
+  receiver.save();
+
+  let whale_sender = Whale.load(event.params.from.toHexString());
+  if (!whale_sender && sender.balance.gt(new BigInt(500000))) {
+    whale_sender = new Whale(event.params.from.toHexString());
+    whale_sender.balance = sender.balance;
+    whale_sender.save();
+  } else if (whale_sender) {
+    whale_sender.balance = sender.balance;
+    whale_sender.save();
+  }
+
+  let whale_receiver = Whale.load(event.params.to.toHexString());
+  if (!whale_receiver && receiver.balance.gt(new BigInt(500000))) {  // > 500k Sushi is a whale
+    whale_receiver = new Whale(event.params.to.toHexString());
+    whale_receiver.balance = receiver.balance;
+    whale_receiver.save();
+  } else if (whale_receiver) {
+    whale_receiver.balance = receiver.balance;
+    whale_receiver.save();
+  }
+  receiver.save();
+  
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
